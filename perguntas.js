1b5899cabbfc20adcf828ee06d5fee7afb74bfb0
mariamatulle <150458744+mariamatulle@users.noreply.github.com>
2024-11-18 14:41:19 +0000

    18112024

--- perguntas.js
+++ perguntas.js
@@ -1,33 +1,33 @@
 criaCartao(
     'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Qual foi o jogador mais importante do Flamengo?',
+    ' Zico (1971 a 1983 e 1985 a 1989)'
 )
 
 criaCartao(
     'Programação',
-    'O que é JavaScript?',
-    'O JavaScript é uma linguagem de programação.'
+    'Quem tem mais títulos pelo Flamengo?',
+    'Gabigol, Arrascaeta e Bruno Henrique igualam Zico e Júnior como jogadores com mais títulos no Flamengo, marcando 13 conquistas.'
 )
 
 criaCartao(
     'Programação',
-    'O que é CSS?',
-    'O CSS é uma lingugem de estilização!'
+    'Qual é o ídolo do Flamengo?',
+    'Gabigol é o principal ídolo de uma nova geração de torcedores rubro-negros.'
 )
 
 criaCartao(
     'Programação',
-    'O que é HTML?',
-    'O HTML é uma lingugem de estruturação e semântica do conteúdo.'
+    'Qual título falta para o Flamengo?',
+    'Flamengo: Copa Sul-Americana. Fluminense: Mundial de Cubes, Copa Sul-Americana, Recopa Sul-Americana e Supercopa do Brasil. Grêmio: Copa Sul-Americana.'
 )
 criaCartao(
     'Programação',
-    'O que é VSCode?',
-    'O Visual Studio Code é um editor de código com diversas integrações.s'
+    'Por que o Flamengo é vermelho?',
+    'Flamengo agora é vermelho e preto, ficou decidido que o vermelho significaria a paixão e a intensidade e o preto a determinação.'
 )
 criaCartao(
     'Programação',
-    'O que é GitHub?',
-    'O GitHub é uma plataforma de desenvolvimento colaborativo que aloja projetos na nuvem.'
+    'Quem tem mais gols no flamengo atualmente?',
+    'Gabigol é o maior artilheiro do Flamengo no século XXI, com 157 gols, sendo fundamental nas conquistas da Libertadores de 2019 e 2022'
 )
