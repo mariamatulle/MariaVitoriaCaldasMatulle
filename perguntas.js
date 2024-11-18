0e2c6987f7eb87eb4b85cf1e45105c3facb7c798
mariamatulle <150458744+mariamatulle@users.noreply.github.com>
2024-11-18 15:29:05 +0000

    18112024

--- perguntas.js
+++ perguntas.js
@@ -1,33 +1,33 @@
 criaCartao(
-    'Programação',
+    '1',
     'Qual foi o jogador mais importante do Flamengo?',
-    ' Zico (1971 a 1983 e 1985 a 1989)'
+    ' Zico (1971 a 1983 e 1985 a 1989).'
 )
 
 criaCartao(
-    'Programação',
+    '2',
     'Quem tem mais títulos pelo Flamengo?',
     'Gabigol, Arrascaeta e Bruno Henrique igualam Zico e Júnior como jogadores com mais títulos no Flamengo, marcando 13 conquistas.'
 )
 
 criaCartao(
-    'Programação',
+    '3',
     'Qual é o ídolo do Flamengo?',
     'Gabigol é o principal ídolo de uma nova geração de torcedores rubro-negros.'
 )
 
 criaCartao(
-    'Programação',
+    '4',
     'Qual título falta para o Flamengo?',
     'Flamengo: Copa Sul-Americana. Fluminense: Mundial de Cubes, Copa Sul-Americana, Recopa Sul-Americana e Supercopa do Brasil. Grêmio: Copa Sul-Americana.'
 )
 criaCartao(
-    'Programação',
+    '5',
     'Por que o Flamengo é vermelho?',
     'Flamengo agora é vermelho e preto, ficou decidido que o vermelho significaria a paixão e a intensidade e o preto a determinação.'
 )
 criaCartao(
-    'Programação',
+    '6',
     'Quem tem mais gols no flamengo atualmente?',
     'Gabigol é o maior artilheiro do Flamengo no século XXI, com 157 gols, sendo fundamental nas conquistas da Libertadores de 2019 e 2022'
 )
